e67d5013cbec36a89ecb52f045e641651b90807b
jorditoda <jordicatalunya@hotmail.com>
2024-09-20 16:35:55 +0200

    first commit

---
+++ src/main/java/com/stormfkrs/someone_like_this/SomeoneLikeThisApplication.java
@@ -0,0 +1,13 @@
+package com.stormfkrs.someone_like_this;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SomeoneLikeThisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SomeoneLikeThisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stormfkrs/someone_like_this/SomeoneLikeThisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stormfkrs.someone_like_this;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SomeoneLikeThisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
